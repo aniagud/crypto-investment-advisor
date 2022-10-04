821e8ca0001a7d385d526f2fabfd1456136b1183
Anna Gudelajtis <anna_gudelajtis@epam.com>
2022-10-04 12:14:37 +0200

    init project

---
+++ src/main/java/com/crypto/cryptoinvestmentadvisor/CryptoInvestmentAdvisorApplication.java
@@ -0,0 +1,13 @@
+package com.crypto.cryptoinvestmentadvisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoInvestmentAdvisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoInvestmentAdvisorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crypto/cryptoinvestmentadvisor/CryptoInvestmentAdvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crypto.cryptoinvestmentadvisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoInvestmentAdvisorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
