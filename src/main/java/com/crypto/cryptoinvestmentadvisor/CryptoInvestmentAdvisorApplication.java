2d038995176c344511a209aaead4f3222a16e57b
Anna Gudelajtis <anna_gudelajtis@epam.com>
2022-10-06 08:27:50 +0200

    Swagger api docs attached

--- src/main/java/com/crypto/cryptoinvestmentadvisor/CryptoInvestmentAdvisorApplication.java
+++ src/main/java/com/crypto/cryptoinvestmentadvisor/CryptoInvestmentAdvisorApplication.java
@@ -2,16 +2,18 @@ package com.crypto.cryptoinvestmentadvisor;
 
 import com.crypto.cryptoinvestmentadvisor.cryptostats.StatsProcessor;
 import com.crypto.cryptoinvestmentadvisor.util.FileReader;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 @ConfigurationPropertiesScan(basePackageClasses = ApplicationProperties.class)
 public class CryptoInvestmentAdvisorApplication {
 
@@ -29,4 +31,12 @@ public class CryptoInvestmentAdvisorApplication {
         return new StatsProcessor(fileReader);
     }
 
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
 }
--- src/main/java/com/crypto/cryptoinvestmentadvisor/cryptostats/CryptoStatsResource.java
+++ src/main/java/com/crypto/cryptoinvestmentadvisor/cryptostats/CryptoStatsResource.java
@@ -1,6 +1,5 @@
-package com.crypto.cryptoinvestmentadvisor;
+package com.crypto.cryptoinvestmentadvisor.cryptostats;
 
-import com.crypto.cryptoinvestmentadvisor.cryptostats.StatsProcessor;
 import com.crypto.cryptoinvestmentadvisor.cryptostats.dto.Instruments;
 import com.crypto.cryptoinvestmentadvisor.cryptostats.dto.MarketSymbol;
 import com.crypto.cryptoinvestmentadvisor.cryptostats.dto.Measures;
@@ -19,18 +18,34 @@ public class CryptoStatsResource {
 
     private final StatsProcessor statsProcessor;
 
+    /**
+     * Return a descending sorted list of all the currently supported cryptos,
+     * comparing the normalized range (i.e. (max-min)/min)
+     * @return Instruments.class
+     */
     @GetMapping("/all-by-range")
     public Instruments getSortedInstruments() {
         return Instruments.of(statsProcessor.getInstrumentsSortedByRange());
     }
 
+
+    /**
+     * Return the oldest/newest/min/max values for a requested crypto
+     * @param market - requested crypto symbol
+     * @return ResponseEntity<Measures>
+     */
     @GetMapping("/markets/{market}")
-    public ResponseEntity<Measures> getMeasuresByCrypto(@PathVariable("market") String market) {
+    public ResponseEntity<Measures> getMeasuresForCrypto(@PathVariable("market") String market) {
         return statsProcessor.getMeasures(market)
                 .map(ResponseEntity::ok)
-                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND)    .build());
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
+    /**
+     * Return the crypto with the highest normalized range for a specific day
+     * @param day - specified day in 'yyyy-MM-dd' format
+     * @return ResponseEntity<MarketSymbol>
+     */
     @GetMapping("/best-for-day")
     public ResponseEntity<MarketSymbol> getBestCryptoForDay(
             @RequestParam("day") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate day) {
